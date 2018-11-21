a9c354014d6fd772447a1f7d83f3945af9c49aba
KevinJustice25 <kevinjustice25@gmail.com>
2018-11-20 19:29:31 -0600

    Added AuthFilter and mappings

---
+++ src/main/java/Servlets/AuthFilter.java
@@ -0,0 +1,33 @@
+package Servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        if (request.getSession()!= null && request.getSession().getAttribute("user")!= null){
+            filterChain.doFilter(servletRequest,servletResponse);
+        } else {
+            request.getRequestDispatcher("/login").forward(request,response);
+        }
+
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
